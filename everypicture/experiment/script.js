88741effb0292778be59a836228b5c6b15834e23
Brian Rodriguez-torres <brodrigueztorres@ucdavis.edu>
2025-02-18 02:38:53 -0800

    push 26

---
+++ everypicture/experiment/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function () {
+    var styleElement = document.createElement("style");
+    styleElement.innerHTML = `
+        #hover:hover {
+            cursor: pointer;
+        }
+        #hover:hover .card-title {
+            text-decoration: underline;
+        }
+    `;
+});
